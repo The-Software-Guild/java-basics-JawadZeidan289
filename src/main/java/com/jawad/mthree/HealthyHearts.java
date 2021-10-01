e3ca4ec068c72d64a5f279a6faea14b0099cfba1
Jawad Zeidan <jawadz@live.co.uk>
2021-10-01 16:08:50 +0100

    004: Complete HealthyHearts task

--- src/main/java/com/jawad/mthree/HealthyHearts.java
+++ src/main/java/com/jawad/mthree/HealthyHearts.java
@@ -1,8 +1,22 @@
 package com.jawad.mthree;
 
+import java.util.Scanner;
+
 public class HealthyHearts {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello there! How old are you currently?");
+
+        int age = scanner.nextInt();
+        int maximum = 220 - age;
+
+        System.out.println("\nYour maximum heart rate should be " + maximum + " beats per minute.");
+
+        int lowerBound = (int) (maximum * 0.5);
+        int upperBound = (int) (maximum * 0.85);
+        System.out.println("Your target heart rate zone is " + lowerBound + " - " + upperBound + " beats per minute.");
 
     }
 
